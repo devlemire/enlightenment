3abddd306963b26eb4258a0e65c640c18b7818cb
James Lemire <webdev.jameslemire@gmail.com>
2024-02-11 20:06:53 -0500

    Bare bones skeleton of API and Client

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const { SERVER_PORT } = process.env;
+const app = express();
+
+app.use(express.static(`${__dirname}/client/build`));
+app.get("*", (req, res, next) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}`);
+});
